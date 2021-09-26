9ce013a48422f6699e6c1e9c5197e5c40dd37371
id3ntity99 <jas06113@gmail.com>
2021-09-26 12:04:18 +0900

    React Event Basic

--- src/index.js
+++ src/index.js
@@ -6,41 +6,49 @@ import "./index.css";
 
 const books = [
   {
+    id: 1,
     title: "System Design Interview",
     author: "Alex Xu",
     img: "https://images-na.ssl-images-amazon.com/images/I/61HlS-BupKL._AC_UL200_SR200,200_.jpg",
   },
   {
+    id: 2,
     title: "Python Crash Course, 2nd Edition",
     author: "Eric Matthes",
     img: "https://images-na.ssl-images-amazon.com/images/I/81vmJCNCm6L._AC_UL200_SR200,200_.jpg",
+  },
+  {
+    id: 3,
+    title: "Introduction to Algorithms",
+    author: "Thomas H. Cormen",
+    img: "https://images-na.ssl-images-amazon.com/images/I/61uRpcdPhNL._AC_UL200_SR200,200_.jpg",
   },
 ];
 function BookList() {
   return (
     <section className="bookList">
-      <Book
-        title={books[0].title}
-        author={books[0].author}
-        img={books[0].img}
-      />
-      <Book
-        title={books[1].title}
-        author={books[1].author}
-        img={books[1].img}
-      />
+      {books.map((book) => {
+        return <Book key={book.id} {...book}></Book>;
+      })}
     </section>
   );
 }
 
-function Book({ img, title, author }) {
-  //const { img, title, author } = props;
+function Book(props) {
+  const { img, title, author } = props;
+
+  const complex = (author, event) => {
+    console.log(author);
+    console.log(event.target);
+  };
   return (
     <article className="book">
       <img src={img} alt="" />
       <h1>{title}</h1>
       <h4>{author}</h4>
-      <p></p>
+      <button type="button" onClick={(event) => complex(author, event)}>
+        Greeting
+      </button>
     </article>
   );
 }
